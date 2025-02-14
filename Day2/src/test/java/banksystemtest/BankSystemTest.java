ef3f5db4ef0184f7e6dc7ff71e0fe98c8da129fd
omya6av <omya6av@gmail.com>
2025-02-14 11:50:42 +0530

    [Om] add. unit testing of banking system

---
+++ Day2/src/test/java/banksystemtest/BankSystemTest.java
@@ -0,0 +1,15 @@
+//package banksystemtest;
+//import bankingsystem.BankSystem;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//public class BankSystemTest {
+//    class BankSystemTest {
+//        @Test
+//        void testAccountCreation() {
+//            BankSystem bank = new BankSystem();
+//            bank.createAccount(1001, 5000.00);
+//            assertEquals(5000.00, bank.getSortedAccounts().firstKey());
+//        }
+//        }
+//    }
+
