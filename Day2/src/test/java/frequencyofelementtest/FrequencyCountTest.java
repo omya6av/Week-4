60a64b27732706a5842ed7845f5ae2a6e018b749
omya6av <omya6av@gmail.com>
2025-02-14 11:51:34 +0530

    [Om] add. Unit testing of frequency of element

---
+++ Day2/src/test/java/frequencyofelementtest/FrequencyCountTest.java
@@ -0,0 +1,24 @@
+package frequencyofelementtest;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import listinterface.frequencyofelement.FrequencyCount;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FrequencyCountTest {
+    @Test
+    void frequencyTest(){
+        List<String> list = Arrays.asList("apple", "banana", "apple", "orange");
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("apple", 2);
+        expected.put("banana", 1);
+        expected.put("orange", 1);
+        assertEquals(expected, FrequencyCount.frequencyCount(list));
+
+    }
+}
