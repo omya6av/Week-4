3d35364aaa07642831573c59260a297fe486239b
omya6av <omya6av@gmail.com>
2025-02-16 00:11:25 +0530

    [Om] add. Solution of extra currency

---
+++ Day5/src/main/java/regex/AdvancedProblems/extractcurrency/ExtractCurrency.java
@@ -0,0 +1,17 @@
+package regex.advancedproblems.extractcurrency;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractCurrency {
+    public static void main(String[] args) {
+        String text = "The price is $45.99, and the discount is 10.50.";
+        String regex = "$?\\d+\\.\\d{2}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+    }
+}
