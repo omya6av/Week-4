d6098c3085e3e41f6957d80841a0e5d3a34dbcaf
omya6av <omya6av@gmail.com>
2025-02-16 22:32:59 +0530

    [Om] add. Solution of Social security number

---
+++ Day5/src/main/java/regex/AdvancedProblems/socialsecuritynumber/SocialSecurityNumber.java
@@ -0,0 +1,20 @@
+package AdvancedProblems.socialsecuritynumber;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SocialSecurityNumber {
+    public static void main(String[] args) {
+
+        String text = "My SSN is 123-45-6789.";
+
+        String regex = "\\b\\d{3}-\\d{2}-\\d{4}\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()){
+            System.out.println(matcher.group() + " is valid");
+        }
+    }
+}
