3e4379167c90215f2d99be74fbec0abbd5a48da7
omya6av <omya6av@gmail.com>
2025-02-16 23:06:50 +0530

    [Om]
    Solution of Extract all email addresses

---
+++ Day5/src/main/java/regex/extractionproblems/extractallemailaddressesfromtext/ExtractAllEmailAddressesfromText.java
@@ -0,0 +1,22 @@
+package extractionproblems.extractallemailaddressesfromtext;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractAllEmailAddressesfromText {
+    public static void main(String[] args) {
+
+        String text = "Contact us at support@example.com and info@company.org";
+
+        String regex = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()){
+            System.out.println("Emails : "+ matcher.group());
+        }
+    }
+}
+
