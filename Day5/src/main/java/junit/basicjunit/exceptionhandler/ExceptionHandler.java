7b6eb1f1c1f6e601321b0a7f46118177b7a015e5
omya6av <omya6av@gmail.com>
2025-02-17 16:53:47 +0530

    [Om] Add. solution of exception handler

---
+++ Day5/src/main/java/junit/basicjunit/exceptionhandler/ExceptionHandler.java
@@ -0,0 +1,15 @@
+package junit.basicjunit.exceptionhandler;
+
+public class ExceptionHandler {
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Division result: " + divide(10, 2)); // 5
+
+    }
+}
