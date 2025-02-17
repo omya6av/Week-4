47a420fc408207dc112533624ed9d4be769968e3
omya6av <omya6av@gmail.com>
2025-02-17 16:54:44 +0530

    [Om]] Add. solution of Data connection

---
+++ Day5/src/main/java/junit/basicjunit/databaseconnection/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package junit.basicjunit.databaseconnection;
+
+public class DatabaseConnection {
+    private boolean isConnected;
+
+    public void connect() {
+        isConnected = true;
+        System.out.println("Database connected.");
+    }
+
+    public void disconnect() {
+        isConnected = false;
+        System.out.println("Database disconnected.");
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+}
+
