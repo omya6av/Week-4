06e01b5b4f897fccb8b5687442dc0607c268ee42
omya6av <omya6av@gmail.com>
2025-02-19 12:03:01 +0530

    [Om] Add. Junit testing of exception handler

---
+++ Day5/src/test/java/junit/basicjunit/exceptionhandlertest/ExceptionHandlerTest.java
@@ -0,0 +1,24 @@
+package junit.basicjunit.exceptionhandlertest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import junit.basicjunit.exceptionhandler.ExceptionHandler;
+import org.junit.jupiter.api.Test;
+
+class ExceptionHandlerTest {
+
+    @Test
+    void testDivision() {
+        assertEquals(5, ExceptionHandler.divide(10, 2));
+        assertEquals(-3, ExceptionHandler.divide(-9, 3));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            ExceptionHandler.divide(10, 0);
+        });
+
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
+}
